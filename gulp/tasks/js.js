3bd39c98bc80ed71170f291c9a2d7be6d35db805
andreiribac <ribacandrei@gmail.com>
2022-02-23 15:46:49 +0200

    js task comments

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,18 +2,21 @@ import webpack from 'webpack-stream';
 
 export const js = () => {
 	return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
+		// Указывает на возникающую ошибку в этом типе файла
 		.pipe(app.plugins.plumber(
 			app.plugins.notify.onError({
 				title: 'JS',
 				message: 'Error: <%= error.message %>'
 			}))
-	)
+		)
 		.pipe(webpack({
-			mode: app.isDev ? 'production': 'development',
+			mode: app.isDev ? 'production' : 'development',
 			output: {
 				filename: 'app.min.js',
 			}
 		}))
+		// Копирование файлов в готовый проект
 		.pipe(app.gulp.dest(app.path.build.js))
+		// Рестарт браузера
 		.pipe(app.plugins.browsersync.stream());
 }
