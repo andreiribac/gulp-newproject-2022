19307fd31182e2911bed705a07f05bf335687ef2
andreiribac <ribacandrei@gmail.com>
2022-02-23 15:15:13 +0200

    add html task comments

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -4,15 +4,20 @@ import versionNumber from 'gulp-version-number';
 
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
+		// Указывает на возникающую ошибку в этом типе файла
 		.pipe(app.plugins.plumber(
 			app.plugins.notify.onError({
 				title: 'HTML',
 				message: 'Error: <%= error.message %>'
 			}))
 		)
+		// Сборка html из компонентов
 		.pipe(fileinclude())
+		// Замена алиасов (относительных путей) для изображения
 		.pipe(app.plugins.replace(/@img\//g, 'img/'))
+		// Формирование тега picture и .webp формата изображения
 		.pipe(webpHtmlNosvg())
+		// Добавление версионности что бы избежать кэширования в браузере
 		.pipe(
 			versionNumber({
 				'value': '%DT%',
@@ -29,6 +34,8 @@ export const html = () => {
 				}
 			})
 		)
+		// Копирование файлов в готовый проект
 		.pipe(app.gulp.dest(app.path.build.html))
+		// Рестарт браузера
 		.pipe(app.plugins.browsersync.stream());
 }
